8f0da5c9f3a62b104b9397a844867b84f07f4375
Casey Visintin <cvisintin@student.unimelb.edu.au>
2017-03-21 17:45:29 +1100

    revise plots

--- R/collision_plots.R
+++ R/collision_plots.R
@@ -7,70 +7,83 @@ invcloglog <- function (x) {1-exp(-exp(x))}
 load("output/coll_glm")
 load("output/coll_model_data")
 
-ggplot <- function(...) { ggplot2::ggplot(...) + theme_bw() + ylab("LIKELIHOOD OF COLLISION") + theme(text = element_text(size=6)) + theme(legend.position="none") }
+ggplot <- function(...) {
+  ggplot2::ggplot(...) + 
+    ylab("LIKELIHOOD OF COLLISION") +
+    theme_bw() +
+    theme(legend.key = element_blank(), legend.position="none") +
+    theme(plot.margin=unit(c(.5,.5,.1,.1),"cm")) +
+    theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+    theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+    theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+    theme(text = element_text(size = 12))
+  }
 
-tiff('figs/egk.tif', pointsize = 6, compression = "lzw", res=300, width = 900, height = 900)
+tiff('figs/egk.tif', pointsize = 20, compression = "lzw", res=300, width = 900, height = 900)
+occ.range <- seq(0,1,1/(nrow(model.data)+1))[-c(1,length(seq(0,1,1/(nrow(model.data)+1))))]
 ggplot(data = model.data) +
-  geom_line(aes(x = egk, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*log(egk) + coef(coll.glm)[3]*mean(log(tvol)) + coef(coll.glm)[4]*mean(log(tspd)) ) ) ) +
-  scale_x_continuous(limits = c(0,1), breaks = seq(0,1,.1)) +
-  scale_y_continuous(limits = c(0,1), breaks = seq(0,1,.1)) +
+  geom_line(aes(x = occ.range, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*log(occ.range) + coef(coll.glm)[3]*mean(log(tvol)) + coef(coll.glm)[4]*mean(log(tspd)) ) ) ) +
+  geom_ribbon(aes(x = occ.range, ymin=invcloglog(
+    coef(coll.glm)[1] +
+      (coef(coll.glm)[2] - coef(summary(coll.glm))[2,2]*1.96)*log(occ.range) +
+      coef(coll.glm)[3]*mean(log(tvol)) +
+      coef(coll.glm)[4]*mean(log(tspd))
+  ),
+  ymax=invcloglog(
+    coef(coll.glm)[1] +
+      (coef(coll.glm)[2] + coef(summary(coll.glm))[2,2]*1.96)*log(occ.range) +
+      coef(coll.glm)[3]*mean(log(tvol)) +
+      coef(coll.glm)[4]*mean(log(tspd))
+  )
+  ), alpha=0.3
+  ) +
   xlab("EGK") +
-  geom_ribbon(aes(x = egk, ymin=invcloglog(
+  scale_x_continuous(limits = c(0,1), expand = c(0, 0), breaks = seq(0,1,.2)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0, 0), breaks = seq(0,1,.1))
+dev.off()
+
+tiff('figs/tvol.tif', pointsize = 20, compression = "lzw", res=300, width = 900, height = 900)
+tvol.range <- seq(0,12500,12500/(nrow(model.data)+1))[-c(1,length(seq(0,12500,12500/(nrow(model.data)+1))))]
+ggplot(data = model.data) +
+  geom_line(aes(x = tvol.range, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*mean(log(egk)) + coef(coll.glm)[3]*log(tvol.range) + coef(coll.glm)[4]*mean(log(tspd)) ) ) ) +
+  geom_ribbon(aes(x = tvol.range, ymin=invcloglog(
     coef(coll.glm)[1] +
-      (coef(coll.glm)[2] - coef(summary(coll.glm))[2,2]*1.96)*log(egk) +
-      coef(coll.glm)[3]*mean(log(tvol)) +
+      coef(coll.glm)[2]*mean(log(egk)) +
+      (coef(coll.glm)[3] - coef(summary(coll.glm))[3,2]*1.96)*log(tvol.range) +
       coef(coll.glm)[4]*mean(log(tspd))
   ),
   ymax=invcloglog(
     coef(coll.glm)[1] +
-      (coef(coll.glm)[2] + coef(summary(coll.glm))[2,2]*1.96)*log(egk) +
-      coef(coll.glm)[3]*mean(log(tvol)) +
+      coef(coll.glm)[2]*mean(log(egk)) +
+      (coef(coll.glm)[3] + coef(summary(coll.glm))[3,2]*1.96)*log(tvol.range) +
       coef(coll.glm)[4]*mean(log(tspd))
   )
   ), alpha=0.3
-  )
-dev.off()
-
-tiff('figs/tvol.tif', pointsize = 6, compression = "lzw", res=300, width = 900, height = 900)
-ggplot(data = model.data) +
-  geom_line(aes(x = tvol, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*mean(log(egk)) + coef(coll.glm)[3]*log(tvol) + coef(coll.glm)[4]*mean(log(tspd)) ) ) ) +
-  scale_x_continuous(limits = c(0,12500), breaks = seq(0,12500,2500)) +
-  scale_y_continuous(limits = c(0,1), breaks = seq(0,1,.1)) +
+  ) +
   xlab("TVOL") +
-  geom_ribbon(aes(x = tvol, ymin=invcloglog(
-    coef(coll.glm)[1] +
-      coef(coll.glm)[2]*mean(log(egk)) +
-      (coef(coll.glm)[3] - coef(summary(coll.glm))[3,2]*1.96)*log(tvol) +
-      coef(coll.glm)[4]*mean(log(tspd))
-  ),
-  ymax=invcloglog(
-    coef(coll.glm)[1] +
-      coef(coll.glm)[2]*mean(log(egk)) +
-      (coef(coll.glm)[3] + coef(summary(coll.glm))[3,2]*1.96)*log(tvol) +
-      coef(coll.glm)[4]*mean(log(tspd))
-  )
-  ), alpha=0.3
-  )
+  scale_x_continuous(limits = c(0,12500), expand = c(0, 0), breaks = seq(0,12500,2500)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0, 0), breaks = seq(0,1,.1))
 dev.off()
 
-tiff('figs/tspd.tif', pointsize = 6, compression = "lzw", res=300, width = 900, height = 900)
+tiff('figs/tspd.tif', pointsize = 20, compression = "lzw", res=300, width = 900, height = 900)
+tspd.range <- tspd.range <- seq(0,100,100/(nrow(model.data)+1))[-c(1,length(seq(0,100,100/(nrow(model.data)+1))))]
 ggplot(data = model.data) +
-  geom_line(aes(x = tspd, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*mean(log(egk)) + coef(coll.glm)[3]*mean(log(tvol)) + coef(coll.glm)[4]*log(tspd) ) ) ) +
-  scale_x_continuous(limits = c(40,100), breaks = seq(40,100,10)) +
-  scale_y_continuous(limits = c(0,1), breaks = seq(0,1,.1)) +
-  xlab("TSPD") +
-  geom_ribbon(aes(x = tspd, ymin=invcloglog(
+  geom_line(aes(x = tspd.range, invcloglog(coef(coll.glm)[1] + coef(coll.glm)[2]*mean(log(egk)) + coef(coll.glm)[3]*mean(log(tvol)) + coef(coll.glm)[4]*log(tspd.range) ) ) ) +
+  geom_ribbon(aes(x = tspd.range, ymin=invcloglog(
     coef(coll.glm)[1] +
       coef(coll.glm)[2]*mean(log(egk)) +
       coef(coll.glm)[3]*mean(log(tvol)) +
-      (coef(coll.glm)[4] - coef(summary(coll.glm))[4,2]*1.96)*log(tspd)
+      (coef(coll.glm)[4] - coef(summary(coll.glm))[4,2]*1.96)*log(tspd.range)
   ),
   ymax=invcloglog(
     coef(coll.glm)[1] +
       coef(coll.glm)[2]*mean(log(egk)) +
       coef(coll.glm)[3]*mean(log(tvol)) +
-      (coef(coll.glm)[4] + coef(summary(coll.glm))[4,2]*1.96)*log(tspd)
+      (coef(coll.glm)[4] + coef(summary(coll.glm))[4,2]*1.96)*log(tspd.range)
   )
   ), alpha=0.3
-  )
+  ) +
+  xlab("TSPD") +
+  scale_x_continuous(limits = c(0,100), expand = c(0, 0), breaks = seq(0,100,20)) +
+  scale_y_continuous(limits = c(0,1), expand = c(0, 0), breaks = seq(0,1,.1))
 dev.off()
